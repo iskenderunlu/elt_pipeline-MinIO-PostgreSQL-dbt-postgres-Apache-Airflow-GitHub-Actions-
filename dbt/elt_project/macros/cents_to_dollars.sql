6317c7daa38aa3f313c9fa6f3eff6b9e004c7726
Iskender Unlu <7967903+iskenderunlu@users.noreply.github.com>
2026-06-25 18:16:03 +0300

    Update cents_to_dollars.sql

--- dbt/elt_project/macros/cents_to_dollars.sql
+++ dbt/elt_project/macros/cents_to_dollars.sql
@@ -1,5 +1,5 @@
--- Örnek macro: kuruşu liraya çevir
--- Kullanım: {{ cents_to_dollars('amount_cents') }}
+-- Example macro: convert penny to lira 
+-- Usage: {{ cents_to_dollars('amount_cents') }}
 
 {% macro cents_to_dollars(column_name, precision=2) %}
     ({{ column_name }} / 100.0)::NUMERIC(12, {{ precision }})
